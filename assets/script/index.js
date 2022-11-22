1b04bfbfcdc3b36a9c4c849ece0b2fa47a27f715
Azam <tarbelite04@gmail.com>
2022-11-22 09:39:19 -0600

    basic structure created

---
+++ assets/script/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+//------------------------------------------------------------------
