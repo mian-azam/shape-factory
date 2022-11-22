2574d65ae442e27b90abd3f30f8f525e7d657b0d
Azam <tarbelite04@gmail.com>
2022-11-22 13:12:01 -0600

    colors changing

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,9 +2,28 @@
 'use strict';
 
 import { onEvent, select } from "./functions.js";
+import { Shapes } from "./shapes.js";
 
 const btn = select('.btn');
+const parent = select('.parent');
+const shapes = select('.shapes');
+const colors = select('.colors');
+
+const myColors = {
+    Blue: '#0f9',
+    Green: '#9f0',
+    Orange: '#f90',
+    Pink: '#f09',
+    Purple: '#90f'
+}
+
+
 
 onEvent('click', btn, function () {
-    btn.style.color = '#fff';
+    let div = document.createElement('div');
+    div.classList.add(shapes.value);
+    parent.appendChild(div);
+    div.style.backgroundColor = myColors[colors.value];
 });
+
+
---
+++ assets/script/shapes.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class Shapes {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    setColor() {
+
+    }
+
+
+    getInfo() {
+
+    }
+}
+
+const colors = {
+    Blue: '#0f9',
+    Green: '#9f0',
+    Orange: '#f90',
+    Pink: '#f09',
+    Purple: '#90f'
+}
+
+export { Shapes };
